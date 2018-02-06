251e7ef380f23c6da8ae7dab088ad4af0a61c053
joe <anjone@126.com>
2018-02-06 16:50:34 +0800

    add fabric test

---
+++ python/fabfile_pip.py
@@ -0,0 +1,10 @@
+from getpass import getpass
+from fabric.api import settings, run, env, prompt
+
+def remote_server():
+    env.hosts = ['127.0.0.1']
+    env.user = prompt('Enter user name: ')
+    env.password = getpass('Enter password: ')
+
+def install_package():
+    run('pip install numpy')
