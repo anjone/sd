ddb8795bb2edf79615e9962be605beaa6d8167ae
joe <anjone@126.com>
2019-11-14 11:25:34 +0800

    add angular local weather test

---
+++ js/local-weather-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { CurrentWeatherComponent } from './components/current-weather/current-weather.component';
+
+import { WeatherService } from './services/weather/weather.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CurrentWeatherComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [WeatherService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
